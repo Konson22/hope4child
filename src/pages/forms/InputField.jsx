7e30f4fa5ab34ce5bf6517de1b60669ffc82f269
Choose life <chooselife2023@gmail.com>
2023-04-11 22:15:53 +0200

    refactor UI

--- src/pages/forms/InputField.jsx
+++ src/pages/forms/InputField.jsx
@@ -5,15 +5,10 @@ export default function InputField({label, key='', ...props}){
     const [field, meta] = useField(props)
 
     return(
-        <div className="mb-4" key={key}>
+        <div className="mb-6" key={key}>
             <label htmlFor={label}>{label}</label>
             <input
-                className={`
-                    lg:h-[3.5rem] h-[3rem] border 
-                    border-gray-300 focus:border-green-500 rounded-xl focus:outline-none 
-                    bg-gray-100 bg-opacity-75 w-full px-3 
-                    ${(meta.touched && meta.error) ? 'border-b-red-500':''}
-                `}
+                className={`lg:h-[3.5rem] h-[3rem] focus:border-b-2 focus:border-b-green-500 focus:outline-none bg-gray-100 rounded w-full  px-3 ${(meta.touched && meta.error) ? 'border-b-red-500':''}`}
                 placeholder={field.placeholder}
                 disabled={props.isLoading}
                 {...field}
